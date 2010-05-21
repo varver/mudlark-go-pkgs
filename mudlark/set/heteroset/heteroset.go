30b04cda1c4aae4b544cecbb63da5d0a0959f294
Peter Williams <pwil3058@gmail.com>
2010-05-21 15:45:04 +1000

    Start implementing heterogeneous sets
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

---
+++ mudlark/set/heteroset/heteroset.go
@@ -0,0 +1,322 @@
+// Copyright 2010 -- Peter Williams, all rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Implement heterogenous sets
+
+// Using 2-3 left Leaning Red Black Binary Trees as the low level component.
+// Based on the Java implementation described by Robert Sedgewick
+// in his paper entitled "Left-leaning Red-Black Trees"
+// available at: <www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf>.
+// The principal difference (other than the conversion to Go) is that the items
+// being inserted combine the roles of both key and value.
+package heteroset
+
+import "reflect"
+
+// The type of potential set items must implement this interface and must
+// satisfy the following formal requirements (where a, b and c are all
+// instances of the same type):
+//	 a.Less(b) implies !b.Less(a)
+//	 a.Less(b) && b.Less(c) implies a.Less(c)
+//	 !a.Less(b) && !b.Less(a) implies a == b
+// This method will only be used when reflect.Typeof() the calling object
+// matches reflect.Typeof() of other.
+type Item interface {
+	Less(other interface{}) bool
+}
+
+// LLRB tree node
+type ll_rb_node struct {
+	item Item
+	left, right *ll_rb_node
+	red bool
+}
+
+func new_ll_rb_node(item Item) *ll_rb_node {
+	node := new(ll_rb_node)
+	node.item = item
+	node.red = true
+	return node
+}
+
+func min(a, b int) int { if a < b { return a }; return b }
+
+func cmp_string(a, b string) int {
+	for i, lim := 0, min(len(a), len(b)); i < lim; i++ {
+		if a[i] < b[i] {
+			return -1
+		} else if a[i] > b[i] {
+			return 1
+		}
+	}
+	return len(a) - len(b)
+}
+
+func cmp_type(a, b interface{}) int {
+	ta := reflect.Typeof(a)
+	tb := reflect.Typeof(b)
+	if ta == tb {
+		return 0
+	}
+	if cp := cmp_string(ta.PkgPath(), tb.PkgPath()); cp != 0 {
+		return cp
+	}
+	return cmp_string(ta.Name(), tb.Name())
+}
+
+func (this *ll_rb_node) compare_item(item Item) int {
+	if ct := cmp_type(this.item, item); ct != 0 {
+		return ct
+	}
+	if this.item.Less(item) {
+		return -1
+	} else if item.Less(this.item) {
+		return 1
+	}
+	return 0
+}
+
+func is_red(node *ll_rb_node) bool { return node != nil && node.red }
+
+func flip_colours(node *ll_rb_node) {
+	node.red = !node.red
+	node.left.red = !node.left.red
+	node.right.red = !node.right.red
+}
+
+func rotate_left(node *ll_rb_node) *ll_rb_node {
+	tmp := node.right
+	node.right = tmp.left
+	tmp.left = node
+	tmp.red = node.red
+	node.red = true
+	return tmp
+}
+
+func rotate_right(node *ll_rb_node) *ll_rb_node {
+	tmp := node.left
+	node.left = tmp.right
+	tmp.right = node
+	tmp.red = node.red
+	node.red = true
+	return tmp
+}
+
+func fix_up(node *ll_rb_node) *ll_rb_node {
+	if is_red(node.right) && !is_red(node.left) {
+		node = rotate_left(node)
+	}
+	if is_red(node.left) && is_red(node.left.left) {
+		node = rotate_right(node)
+	}
+	if is_red(node.left) && is_red(node.right) {
+		flip_colours(node)
+	}
+	return node
+}
+
+func insert(node *ll_rb_node, item Item) (*ll_rb_node, bool) {
+	if node == nil {
+		return new_ll_rb_node(item), true
+	}
+	inserted := false
+	switch cmp := node.compare_item(item); {
+	case cmp > 0:
+		node.left, inserted = insert(node.left, item)
+	case cmp < 0:
+		node.right, inserted = insert(node.right, item)
+	default:
+	}
+	return fix_up(node), inserted
+}
+
+func move_red_left(node *ll_rb_node) *ll_rb_node {
+	flip_colours(node)
+	if (is_red(node.right.left)) {
+		node.right = rotate_right(node.right)
+		node = rotate_left(node)
+		flip_colours(node)
+	}
+	return node
+}
+
+func move_red_right(node *ll_rb_node) *ll_rb_node {
+	flip_colours(node)
+	if (is_red(node.left.left)) {
+		node = rotate_right(node)
+		flip_colours(node)
+	}
+	return node
+}
+
+func delete_left_most(node *ll_rb_node) *ll_rb_node {
+	if node.left == nil {
+		return nil
+	}
+	if !is_red(node.left) && !is_red(node.left.left) {
+		node = move_red_left(node)
+	}
+	node.left = delete_left_most(node.left)
+	return fix_up(node)
+}
+
+func delete(node *ll_rb_node, item Item) (*ll_rb_node, bool) {
+	var deleted bool
+	if node.compare_item(item) > 0 {
+		if !is_red(node.left) && !is_red(node.left.left) {
+			node = move_red_left(node)
+		}
+		node.left, deleted = delete(node.left, item)
+	} else {
+		if is_red(node.left) {
+			node = rotate_right(node)
+		}
+		if node.compare_item(item) == 0 && node.right == nil {
+			return nil, true
+		}
+		if !is_red(node.right) && !is_red(node.right.left) {
+			node = move_red_right(node)
+		}
+		if node.compare_item(item) == 0 {
+			left_most := node.right
+			for left_most.left != nil {
+				left_most = left_most.left
+			}
+			node.item = left_most.item
+			node.right = delete_left_most(node.right)
+			deleted = true
+		} else {
+			node.right, deleted = delete(node.right, item)
+		}
+	}
+	return fix_up(node), deleted
+}
+
+// Iteration using recursion is safe because the depth of the tree should never
+// be greater than 2Log2(N) where N is the number of nodes in the tree and
+// (in general) will be approximately Log2(N).
+
+func iterate_preorder(node *ll_rb_node, c chan<- Item) {
+	if node == nil {
+		return
+	}
+	c <- node.item
+	iterate_preorder(node.left, c)
+	iterate_preorder(node.right, c)
+}
+
+func iterate(node *ll_rb_node, c chan<- Item) {
+	iterate_preorder(node, c)
+	close(c)
+}
+
+func copy(node *ll_rb_node) *ll_rb_node {
+	if node == nil { return nil }
+	clone := new(ll_rb_node)
+	clone.item = node.item
+	clone.red = node.red
+	clone.left = copy(node.left)
+	clone.right = copy(node.right)
+	return clone
+}
+
+type ll_rb_tree struct {
+	root *ll_rb_node
+	count uint
+}
+
+func (this ll_rb_tree) find(item Item) (found bool, iterations uint) {
+	if this.count == 0 {
+		return
+	}
+	for node := this.root; node != nil && !found; {
+		iterations++
+		switch cmp := node.compare_item(item); {
+		case cmp > 0:
+			node = node.left
+		case cmp < 0:
+			node = node.right
+		default:
+			found = true
+		}
+	}
+	return
+}
+
+func (this *ll_rb_tree) insert(item Item) {
+	var inserted bool
+	this.root, inserted = insert(this.root, item)
+	if inserted {
+		this.count++
+	}
+	this.root.red = false
+}
+
+func (this *ll_rb_tree) delete(item Item) {
+	var deleted bool
+	this.root, deleted = delete(this.root, item)
+	if deleted {
+		this.count--
+	}
+	this.root.red = false
+}
+
+func (this ll_rb_tree) iterator() <-chan Item {
+	c := make(chan Item)
+	go iterate(this.root, c)
+	return c
+}
+
+// Set is a set of hetrogeneous objects whos types implement the Item
+// interface. Instances of Set must be initialized using Make()
+// before use.  E.g.:
+//	var s Set = heteroset.Make(item1, ....)
+type Set struct {
+	Data *ll_rb_tree
+}
+
+// Make a Set. The optional Item parameters will be used to initialize the set's
+// contents.
+func Make(items ...Item) (set Set) {
+	set.Data = new(ll_rb_tree)
+	for _, item := range items {
+		set.Data.insert(item)
+	}
+	return
+}
+
+// Make a copy of this set.
+func (this Set) Copy() (set Set) {
+	set.Data.root = copy(this.Data.root)
+	set.Data.count = this.Data.count
+	return
+}
+
+// Len returns the number of items in the set.
+func (this Set) Len() uint {
+	return this.Data.count
+}
+
+// Add an item to the set.
+func (this *Set) Add(item Item) {
+	this.Data.insert(item)
+}
+
+// Remove item from the set.
+func (this *Set) Remove(item Item) {
+	this.Data.delete(item)
+}
+
+// Is there an instance equal to item in the set.
+func (this *Set) Has(item Item) (found bool) {
+	found, _ = this.Data.find(item)
+	return
+}
+
+// Iterate over the set members in unspecified order.
+func (this Set) Iter() <-chan Item {
+	return this.Data.iterator()
+}
+
+
---
+++ mudlark/set/heteroset/heteroset_test.go
@@ -0,0 +1,183 @@
+// Copyright 2010 -- Peter Williams, all rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package heteroset
+
+import (
+	"testing"
+	"rand"
+	"reflect"
+	"fmt"
+)
+
+type Int int
+
+func (i Int) Less(other interface{}) bool {
+	return int(i) < int(other.(Int))
+}
+
+type Real float64
+
+func (r Real) Less(other interface{}) bool {
+	return float64(r) < float64(other.(Real))
+}
+
+func print_node(node *ll_rb_node) {
+	if node == nil { return }
+	fmt.Printf("%v\n", node)
+	print_node(node.left)
+	print_node(node.right)
+}
+
+func TestMakell_rb_tree(t *testing.T) {
+	var tree ll_rb_tree
+	if reflect.Typeof(tree).String() != "heteroset.ll_rb_tree" {
+		t.Errorf("Expected type \"heteroset.ll_rb_tree\": got %v", reflect.Typeof(tree).String())
+	}
+	if tree.count != 0 {
+		t.Errorf("Expected bitcount 0: got %v", tree.count)
+	}
+	if tree.root != nil {
+		t.Errorf("Root is not nil")
+	}
+	found, iterations := tree.find(Int(1))
+	if found {
+		t.Errorf("Unexpectedly found Int")
+	}
+	if iterations != 0 {
+		t.Errorf("Expected 0 iteretions got: %v", iterations)
+	}
+	found, iterations = tree.find(Real(1.0))
+	if found {
+		t.Errorf("Unexpectedly found Real")
+	}
+	if iterations != 0 {
+		t.Errorf("Expected 0 iteretions got: %v", iterations)
+	}
+}
+
+func TestMakell_rb_tree_ptr(t *testing.T) {
+	tree := new(ll_rb_tree)
+	if reflect.Typeof(tree).String() != "*heteroset.ll_rb_tree" {
+		t.Errorf("Expected type \"*heteroset.ll_rb_tree\": got %v", reflect.Typeof(tree).String())
+	}
+	if tree.count != 0 {
+		t.Errorf("Expected bitcount 0: got %v", tree.count)
+	}
+	if tree.root != nil {
+		t.Errorf("Root is not nil")
+	}
+	found, iterations := tree.find(Int(1))
+	if found {
+		t.Errorf("Unexpectedly found Int")
+	}
+	if iterations != 0 {
+		t.Errorf("Expected 0 iteretions got: %v", iterations)
+	}
+	found, iterations = tree.find(Real(1.0))
+	if found {
+		t.Errorf("Unexpectedly found Real")
+	}
+	if iterations != 0 {
+		t.Errorf("Expected 0 iteretions got: %v", iterations)
+	}
+}
+
+func TestMakeinsert(t *testing.T) {
+	var tree ll_rb_tree
+	var failures int
+	for i := 0; i < 1000; i++ {
+		iitem := Int(rand.Intn(800))
+		iin, _ := tree.find(iitem)
+		tsz := tree.count
+		tree.insert(iitem)
+		if iin {
+			if tsz != tree.count {
+				t.Errorf("Count changed (insert i): Expected %v got: %v", tsz, tree.count)
+			}
+		} else {
+			if tsz + 1 != tree.count {
+				t.Errorf("Count uchanged (insert i): Expected %v got: %v", tsz + 1, tree.count)
+			}
+		}
+		if iin, _ = tree.find(iitem); !iin {
+			t.Errorf("Inserted %v not found", iitem)
+			failures++
+		}
+		ritem := Real(rand.Float64())
+		rin, _:= tree.find(ritem)
+		tsz = tree.count
+		tree.insert(ritem)
+		if rin {
+			if tsz != tree.count {
+				t.Errorf("Count changed (insert i): Expected %v got: %v", tsz, tree.count)
+			}
+		} else {
+			if tsz + 1 != tree.count {
+				t.Errorf("Count uchanged (insert i): Expected %v got: %v", tsz + 1, tree.count)
+			}
+		}
+		if rin, _ = tree.find(ritem); !rin {
+			t.Errorf("Inserted %v not found", ritem)
+			failures++
+		}
+	}
+	if failures != 0 {
+		t.Errorf("%v failures", failures)
+	}
+}
+
+func TestMakeiterate(t *testing.T) {
+	var tree ll_rb_tree
+	var count int
+	for i := 0; i < 10000; i++ {
+		tree.insert(Int(rand.Int()))
+		count++
+		tree.insert(Real(rand.Float64()))
+		count++
+	}
+	for item := range tree.iterator() {
+		if cmp_type(item, Int(0)) == 0 {
+			// shut compiler up
+		}
+		count--
+	}
+	if count != 0 {
+		t.Errorf("%v count", count)
+	}
+}
+
+func max_depth(node *ll_rb_node) uint {
+	if node == nil { return 0 }
+	ld := max_depth(node.left)
+	rd := max_depth(node.right)
+	if ld > rd {
+		return ld + 1
+	}
+	return rd + 1
+}
+
+// test that depth of tree doesn't exceed 2 * log2(cardinality) using:
+//		random (best case) input
+//		sequential (worst case) input
+func TestMakedepth_properties(t *testing.T) {
+	var tree_sequential, tree_reverse, tree_random ll_rb_tree
+	var i int
+	var max_depth_sequential, max_depth_reverse, max_depth_random uint
+	for n := uint(1); n < 16; n++ {
+		N := 1 << n
+		for ; i < N; i++ {
+			tree_sequential.insert(Int(i))
+			tree_reverse.insert(Int(N - i))
+			tree_random.insert(Int(rand.Int()))
+		}
+		max_depth_sequential = max_depth(tree_sequential.root)
+		max_depth_reverse = max_depth(tree_reverse.root)
+		max_depth_random  = max_depth(tree_random.root)
+		if max_depth_sequential > 2 * n || max_depth_reverse > 2 * n || max_depth_random > 2 * n {
+			t.Errorf("%v : %v : %v : %v\n", n, i, max_depth_sequential, max_depth_random)
+		}
+	}
+}
+
