062093a14802bfe479958042873d5399c80bf198
Peter Williams <pwil3058@gmail.com>
2010-05-24 11:50:14 +1000

    Change license on all files to "new BSD"
    Signed-off-by: Peter Williams <pwil3058@gmail.com>

--- mudlark/sort/sort_test.go
+++ mudlark/sort/sort_test.go
@@ -1,6 +1,5 @@
 // Copyright 2010 -- Peter Williams, all rights reserved
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Use of this source code is governed by the new BSD license.
 
 package sort_test
 
